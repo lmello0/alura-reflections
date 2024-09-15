cfc2681262474681fb1ba6d035a29f61159aa87b
lmello <melloluc02@gmail.com>
2024-09-15 11:22:31 -0300

    :sparkles: feat: Add Transformator class to convert entities into DTOs

--- src/main/java/br/com/alura/lmello/PessoaService.java
+++ src/main/java/br/com/alura/lmello/PessoaService.java
@@ -1,10 +1,16 @@
 package br.com.alura.lmello;
 
+import br.com.alura.lmello.reflection.Transformator;
+
+import java.lang.reflect.InvocationTargetException;
+
 public class PessoaService {
 
-    public PessoaDTO list() {
+    public PessoaDTO list() throws ClassNotFoundException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
         Pessoa pessoa = new PessoaRepository().list();
 
-        return new PessoaDTO(pessoa.getNome(), pessoa.getCpf());
+        PessoaDTO pessoaDTO = new Transformator().transform(pessoa);
+
+        return pessoaDTO;
     }
 }
